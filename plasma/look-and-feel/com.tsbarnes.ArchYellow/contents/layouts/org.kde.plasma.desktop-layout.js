4b45742e5dc35963422096e23590370b573fa88a
Thea Barnes <thea@tsbarnes.com>
2021-08-16 10:16:25 -0400

    Initial commit

---
+++ plasma/look-and-feel/com.tsbarnes.ArchYellow/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,46 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1920x1080": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/ConfigDialog": {
+                    "DialogHeight": "535",
+                    "DialogWidth": "712"
+                },
+                "/Configuration": {
+                    "PreloadWeight": "0"
+                },
+                "/General": {
+                    "ToolBoxButtonState": "topcenter",
+                    "ToolBoxButtonX": "589",
+                    "sortMode": "-1"
+                },
+                "/Wallpaper/org.kde.color/General": {
+                    "Color": "249,166,209"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "/usr/share/wallpapers/ArchLiquid/",
+                    "SlidePaths": "/usr/share/wallpapers"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
